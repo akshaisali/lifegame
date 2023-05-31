5e77a774579046bc8df117c9600242abbb38e94b
Akshaisali <akshaisali007@gmail.com>
2023-05-31 08:18:43 +0530

    green for display_board_alternative

--- test_lifegame.py
+++ test_lifegame.py
@@ -17,5 +17,11 @@ def test_display_board_alive():
     assert lifegame.display_board(board) == expected_output
 
 
+def test_display_board_alternative():
+    board = [[1,1,1],
+    [0,0,0],
+    [1,1,1]]
 
+    expected_output = '###\n***\n###'
+    assert lifegame.display_board(board) == expected_output
   
