aa949de7bae9394dd2182bf7d44a45c2c0cbabbd
Akshaisali <akshaisali007@gmail.com>
2023-05-31 10:37:38 +0530

    green add some changes in display board

--- test_lifegame.py
+++ test_lifegame.py
@@ -3,25 +3,25 @@
 import lifegame
 
 def test_display_board_dead():
-    board = [[0, 0, 0],[0,0,0],[0,0,0],[0, 0, 0], [0, 0, 0]]
-    expected_output = '***\n***\n***\n***\n***'
+    board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0], [0,0,0,0,0]]
+    expected_output = '*****\n*****\n*****\n*****\n*****'
     assert lifegame.display_board(board) == expected_output
 
 def test_display_board_alive():
-    board = [[1,1,1],
-    [1,1,1],
-    [1,1,1],[1,1,1],[1,1,1]]
-    expected_output = '###\n###\n###\n###\n###'
+    board = [[1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]]
+    expected_output = '#####\n#####\n#####\n#####\n#####'
     assert lifegame.display_board(board) == expected_output
 
 
 def test_display_board_alternative():
-    board = [[1,1,1],
-    [0,0,0],
-    [1,1,1],
-    [0,0,0],
-    [0,0,0],]
+    board = [[1,1,1,1,1],
+    [0,0,0,0,0],
+    [1,1,1,1,1],
+    [0,0,0,0,0],
+    [0,0,0,0,0],]
 
-    expected_output = '###\n***\n###\n***\n***'
+    expected_output = '#####\n*****\n#####\n*****\n*****'
     assert lifegame.display_board(board) == expected_output
   
