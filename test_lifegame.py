3b02fdee449e58b3e2d0508dce7c8a8d8880fe64
Akshaisali <akshaisali007@gmail.com>
2023-06-02 10:30:22 +0530

    Green :test finished

--- test_lifegame.py
+++ test_lifegame.py
@@ -1,20 +1,25 @@
-import pytest
 import lifegame
 
 
-def test_create_grid():
-    size = 5
-    prob = 0.5
-    grid = lifegame.create_grid(size, prob)
-    assert len(grid) == size
-    for row in grid:
-        assert len(row) == size
+def test_gmeoflfe_display_dead():
+    assert lifegame.display_grid([[1,1,1],[1,1,1],[1,1,1,]]) == '111\n111\n111'
 
-def test_get_neighbor():
-    board=[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
-    assert lifegame.get_neighbors(board,1,1)==2
+def test_gmeoflfe_display_dead_live():
+    assert lifegame.display_grid([[1,1,1],[1,1,0],[1,1,0]]) == '111\n110\n110'
 
-def test_calculate_next_state():
-    cell = 1
-    neighbors = [1, 0, 1, 0, 0, 1, 1, 0]
-    assert lifegame.calculate_next_state(cell, neighbors) == 1
+
+
+# def test_gmeoflfe_play():
+#       assert lifegame.ply_game([[1,1,1,1,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,1,1],[1,1,1,1,1]]) == '#####\n#####\n#####\n#####\n#####\n'
+
+def sample_board():
+    return [[0,1,0],[0,0,1],[1,1,1]]
+
+def test_next_live_or_dead():
+    expected_board = [[0,0,0],[1,0,1],[0,1,1]]
+    board = sample_board()
+    assert lifegame.next_generation(board) == expected_board
+
+def test_nieghbours_live_dead():
+    board = [[0,1,0],[0,1,0],[0,1,0]]
+    assert lifegame.count_live_dead_neighbors(board,1,1) == 2
